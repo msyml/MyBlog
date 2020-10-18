670bad6e29964b95c00610232975c37a78679cd9
msyml <18858713997@163.com>
2020-10-18 21:36:11 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="header"></div>
+    <div class="body">
+      <div class="aside"></div>
+      <div class="main"></div>
+    </div>
+    <div class="fotter"></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  height: 10%;
+  background: black;
+  width: 100%;
+}
+</style>
