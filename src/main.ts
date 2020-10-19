0b337622b57f4a4913831800e400645033206f58
chenhai <chenhai@lonsid.cn>
2020-10-19 16:34:49 +0800

    引入AntdUI

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import App from './App.vue'
 import './index.css'
 import router from './router/index'
 import store from './store/index'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css';
+
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(Antd)
 app.mount('#app')
